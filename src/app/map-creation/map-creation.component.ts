7492fac85757b852a97ea736452b966d319e8752
RaveTheN <peppereres@live.it>
2023-07-26 11:05:47 +0200

    /map-creation

--- src/app/map-creation/map-creation.component.ts
+++ src/app/map-creation/map-creation.component.ts
@@ -3,5 +3,6 @@ import { Component } from "@angular/core";
 @Component({
   selector: "ngx-map-creation",
   template: ` <router-outlet></router-outlet> `,
+  styleUrls: ["map-creation.component.scss"],
 })
 export class MapCreationComponent {}
