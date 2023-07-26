e4de517a64ee18f34257cd2707f637325a6dadaa
RaveTheN <peppereres@live.it>
2023-07-26 17:51:59 +0200

    /map-creation fuori da pages

---
+++ src/app/map-creation/stepper/map/map.component.ts
@@ -0,0 +1,36 @@
+import { Component, AfterViewInit } from "@angular/core";
+import * as L from "leaflet";
+
+@Component({
+  selector: "ngx-map",
+  templateUrl: "./map.component.html",
+  styleUrls: ["./map.component.css"],
+})
+export class MapComponent implements AfterViewInit {
+  private map: any;
+
+  private initMap(): void {
+    this.map = L.map("map", {
+      center: [37.864, 13.4651],
+      zoom: 16,
+    });
+
+    const tiles = L.tileLayer(
+      "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+      {
+        maxZoom: 18,
+        minZoom: 3,
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      }
+    );
+
+    tiles.addTo(this.map);
+  }
+
+  constructor() {}
+
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
+}
