693b9123576c67ad3c92d2118e7d221913a3b71e
RaveTheN <peppereres@live.it>
2023-08-24 12:31:58 +0200

    provando a estrapolare i punti

--- src/app/pages/create-layer/create-layer.component.ts
+++ src/app/pages/create-layer/create-layer.component.ts
@@ -25,53 +25,6 @@ export class CreateLayerComponent implements OnInit {
     iconSize: [15, 35], // size of the icon
   });
 
-  //list of parks
-  stadspark: L.Marker = L.marker([51.21227, 4.41433], {
-    icon: this.customIcon,
-  }).bindPopup("This is Stadspark");
-  hobokense_polder: L.Marker = L.marker([51.19121, 4.34971], {
-    icon: this.customIcon,
-  }).bindPopup("This is Hobokense Polder");
-  het_rot: L.Marker = L.marker([51.22318, 4.36092], {
-    icon: this.customIcon,
-  }).bindPopup("This is Het Rat");
-  kaisaniemen: L.Marker = L.marker([60.174718, 24.949741], {
-    icon: this.customIcon,
-  }).bindPopup("Kaisaniemen");
-  tahtitornin: L.Marker = L.marker([60.162666, 24.950495], {
-    icon: this.customIcon,
-  }).bindPopup("Tahtitornin");
-  luattasari: L.Marker = L.marker([60.162292, 24.883466], {
-    icon: this.customIcon,
-  }).bindPopup("Luattasari");
-  mendicague: L.Marker = L.marker([43.463164, -3.826884], {
-    icon: this.customIcon,
-  }).bindPopup("Mendicague");
-  dr_morales: L.Marker = L.marker([43.45525, -3.838943], {
-    icon: this.customIcon,
-  }).bindPopup("Parque del dr. Morales");
-  libertad: L.Marker = L.marker([43.470762, -3.808859], {
-    icon: this.customIcon,
-  }).bindPopup("Caje de la libertad");
-
-  private faulty_bench: L.LayerGroup = L.layerGroup([
-    this.stadspark,
-    this.kaisaniemen,
-    this.mendicague,
-  ]);
-
-  private parks: L.LayerGroup = L.layerGroup([
-    this.hobokense_polder,
-    this.tahtitornin,
-    this.dr_morales,
-  ]);
-
-  private churches: L.LayerGroup = L.layerGroup([
-    this.het_rot,
-    this.luattasari,
-    this.libertad,
-  ]);
-
   //forms declaration
   firstForm: FormGroup;
   secondForm: FormGroup;
@@ -79,13 +32,15 @@ export class CreateLayerComponent implements OnInit {
 
   //Radio options for step 1
   options = [
-    { value: [60.1699, 24.9384], label: "Helsinki" },
-    { value: [51.2213, 4.4051], label: "Antwerp" },
-    { value: [43.462776, -3.805], label: "Santander" },
+    { value: [[60.1699, 24.9384], "Helsinki"], label: "Helsinki" },
+    { value: [[51.2213, 4.4051], "Antwerp"], label: "Antwerp" },
+    { value: [[43.462776, -3.805], "Santander"], label: "Santander" },
   ];
   option: any;
 
   popup = L.popup();
+
+  public stocazzo = 0;
 
   //filter's map rendering ---------------------------------------------------------------->
   private map: L.Map;
@@ -98,10 +53,17 @@ export class CreateLayerComponent implements OnInit {
 
   private initFiltersMap(): void {
     this.map = L.map("map", {
-      center: this.option,
+      center: this.option[0],
       zoom: 12,
       layers: [this.osm],
     });
+
+    var circleArea = {
+      point: { latitude: "", longitude: "" },
+      radius: 0,
+    };
+
+    this.stocazzo = 2;
 
     // Initialise the FeatureGroup to store editable layers
     var editableLayers = new L.FeatureGroup();
@@ -129,8 +91,6 @@ export class CreateLayerComponent implements OnInit {
       //empty array, for storing polygons' edges' coordinates
       let edges = [];
 
-      var point = [51.21227, 4.41433];
-
       if (Array.isArray(layer._latlngs)) {
         // For polygons, layer._latlngs[i] is an array of LatLngs objects
         for (let i = 0; i < layer._latlngs.length; i++) {
@@ -143,52 +103,35 @@ export class CreateLayerComponent implements OnInit {
             // edges can then be stored and pushed to backend
           }
         }
-      } else {
         console.log(layer._latlngs);
+        console.log();
+      } else {
+        console.log(layer._latlng);
+        circleArea.point.latitude = layer._latlng.lat;
+        circleArea.point.longitude = layer._latlng.lng;
+        circleArea.radius = layer._radius;
+        console.log(circleArea);
       }
 
-      //questa parte non è necessaria poiché verrà fatta in backend
-      function pointInsidePolygon(point, polygon) {
-        // Extract x and y coordinates of the point
-        const x = point[0];
-        const y = point[1];
-
-        // Initialize a variable to keep track of the number of intersections
-        let numIntersections = 0;
-
-        // Iterate through each edge of the polygon
-        for (let i = 0; i < polygon.length; i++) {
-          const p1 = polygon[i];
-          const p2 = polygon[(i + 1) % polygon.length];
-
-          // Check if the point is on the same y-coordinate as the edge
-          if (y > Math.min(p1.y, p2.y) && y <= Math.max(p1.y, p2.y)) {
-            // Check if the point is to the left of the edge (use cross product)
-            const crossProduct =
-              (x - p1.x) / (p2.x - p1.x) - (y - p1.y) / (p2.y - p1.y);
-            if (crossProduct < 0) {
-              numIntersections++;
-            }
-          }
-        }
-
-        // If the number of intersections is odd, the point is inside the polygon
-        return numIntersections % 2 === 1;
-      }
-
-      //da qui devo prendere le aree disegnate
-      //console.log(editableLayers);
+      console.log(editableLayers);
     });
   }
 
   constructor(private fb: FormBuilder) {}
+
+  //object in which to store the data input by the user--------------------------->
+  queryDetails = {
+    city: "",
+    filters: [],
+    point: {},
+  };
 
   //final map rendering---------------------------------------------------------->
   public finalMap: L.Map;
 
   public initFinalMap(): void {
     this.map = L.map("map", {
-      center: this.option,
+      center: this.option[0],
       zoom: 12,
       layers: [this.osm],
     });
@@ -196,7 +139,7 @@ export class CreateLayerComponent implements OnInit {
     //layer control lets you select which layers you want to see
     const layerControl = L.control.layers(null, null).addTo(this.map);
 
-    this.projectDetails.filters.forEach((element) => {
+    this.queryDetails.filters.forEach((element) => {
       layerControl.addOverlay(element[0], element[1]);
 
       //adding layers so that it is default active in the layerControl
@@ -207,19 +150,6 @@ export class CreateLayerComponent implements OnInit {
     var editableLayers = new L.FeatureGroup();
     this.map.addLayer(editableLayers);
   }
-
-  //object in which to store the data input by the user--------------------------->
-  projectDetails: {
-    city: string;
-    projectName: string;
-    description: string;
-    filters: number[] | string[];
-  } = {
-    city: "",
-    projectName: "",
-    description: "",
-    filters: [],
-  };
 
   //OnInit----------------------------------------------------------------------->
   ngOnInit() {
@@ -243,21 +173,20 @@ export class CreateLayerComponent implements OnInit {
   citySelected: boolean = true;
   onFirstSubmit() {
     this.citySelected = false;
-    console.log(this.firstForm);
+    this.queryDetails.city = this.option[1];
+    console.log(this.queryDetails);
   }
 
   //variable to hide the alert when selecting a filter
   filterSelected: boolean = true;
   onSecondSubmit() {
+    console.log(this.map);
     this.filterSelected = false;
-    this.projectDetails.filters = this.selectedFilters;
-    console.log(this.projectDetails);
+    this.queryDetails.filters = this.selectedFilters;
+    console.log(this.queryDetails);
   }
 
-  onThirdSubmit() {
-    this.projectDetails.projectName = this.firstForm.value.projectName;
-    this.projectDetails.description = this.firstForm.value.description;
-  }
+  onThirdSubmit() {}
 
   //Stepper controls---------------------------------------------------------------->
 
@@ -289,11 +218,7 @@ export class CreateLayerComponent implements OnInit {
   //filters checkbox---------------------------------------------------------------->
 
   //this array serves as a token for the one that will be received from the backend
-  filters = [
-    [this.parks, "parks"],
-    [this.churches, "churches"],
-    [this.faulty_bench, "Faulty benches"],
-  ];
+  filters = ["AirQualityObserved"];
 
   onChange(f: string) {
     this.selectedFilters.includes(f)
