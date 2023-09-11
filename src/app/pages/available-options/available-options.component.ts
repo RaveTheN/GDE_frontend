16093aed87c2fddf80e8b731c0e1ac16c9553bcf
RaveTheN <peppereres@live.it>
2023-09-11 17:43:09 +0200

    add style first page

--- src/app/pages/available-options/available-options.component.ts
+++ src/app/pages/available-options/available-options.component.ts
@@ -6,7 +6,26 @@ import { Component } from "@angular/core";
   styleUrls: ["./available-options.component.scss"],
 })
 export class AvailableOptionsComponent {
-  previousSearches: string[] = ["Search 1", "Search 2", "Search 3"];
+  previousSearches: any = [
+    {
+      name: "Project 1",
+      id: "sim-122412fd-36bc-4f2q-8bwgf6y-dkjn44dc32",
+      date: "2023-09-11",
+      completed: true,
+    },
+    {
+      name: "Project 2",
+      id: "sim-122412fd-36bc-4f2q-8bwgf6y-dkjn44dc33",
+      date: "2023-09-12",
+      completed: false,
+    },
+    {
+      name: "Project 3",
+      id: "sim-122412fd-36bc-4f2q-8bwgf6y-dkjn44dc34",
+      date: "2023-09-13",
+      completed: true,
+    },
+  ];
 
   constructor() {}
 }
