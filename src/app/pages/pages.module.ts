f6b8428ec469f4314885e4b7f8f69c0814b13de2
RaveTheN <peppereres@live.it>
2023-09-11 11:46:40 +0200

    cleaning unused components

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -9,8 +9,6 @@ import {
 
 import { ThemeModule } from "../@theme/theme.module";
 import { PagesComponent } from "./pages.component";
-import { DashboardModule } from "./dashboard/dashboard.module";
-import { ECommerceModule } from "./e-commerce/e-commerce.module";
 import { PagesRoutingModule } from "./pages-routing.module";
 import { MiscellaneousModule } from "./miscellaneous/miscellaneous.module";
 import { CreateLayerComponent } from "./create-layer/create-layer.component";
@@ -28,8 +26,6 @@ import { AvailableOptionsComponent } from "./available-options/available-options
     PagesRoutingModule,
     ThemeModule,
     NbMenuModule,
-    DashboardModule,
-    ECommerceModule,
     MiscellaneousModule,
   ],
   declarations: [
