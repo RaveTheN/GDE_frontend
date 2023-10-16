0a7c58a556ae074fbff32f4dc46c6e0a3ed0dd73
RaveTheN <peppereres@live.it>
2023-10-16 12:02:04 +0200

    changed icon for the markers

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -16,7 +16,8 @@ export class ApiService {
     iconAnchor: [10, 41],
     popupAnchor: [2, -40],
     // specify the path here
-    iconUrl: "https://unpkg.com/leaflet@1.4.0/dist/images/marker-icon.png",
+    iconUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/8/88/Map_marker.svg",
     shadowUrl: "https://unpkg.com/leaflet@1.4.0/dist/images/marker-shadow.png",
   });
 
