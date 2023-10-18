fe07bb46de9fe6ecc738205e4e99c3fe18b4852f
RaveTheN <peppereres@live.it>
2023-10-18 10:59:36 +0200

    add spinning on view-layer component

--- src/app/pages/view-layer/view-layer.component.ts
+++ src/app/pages/view-layer/view-layer.component.ts
@@ -12,6 +12,8 @@ export class ViewLayerComponent implements OnInit {
   // Define map and overlayMaps as class properties with appropriate types.
   private map: L.Map;
   private overlayMaps: { [key: string]: L.Layer } = {};
+  //controls spinner
+  loading: boolean = false;
 
   // Define the OpenStreetMap (osm) layer.
   private osm: L.TileLayer = L.tileLayer(
@@ -51,11 +53,13 @@ export class ViewLayerComponent implements OnInit {
    */
   async ngOnInit() {
     try {
+      this.loading = true;
       // Fetch data from the API.
       let data: any = await this.apiServices.getSearch(
         this.apiServices.currentId
       );
       this.overlayMaps = this.apiServices.apiPoints;
+      this.loading = false;
       switch (data.city) {
         case "Helsinki":
           this.centerCityFromApi = [60.1699, 24.9384];
@@ -69,6 +73,7 @@ export class ViewLayerComponent implements OnInit {
       }
     } catch (error) {
       // Handle API call failure.
+      this.loading = false;
       console.error("API call failed:", error);
     }
 
